afc0191353f4d7ea665ed6f699cf1ce23b409d26
Aegdae <jonnathasg@gmail.com>
2025-07-25 23:06:52 -0300

    att gateway

---
+++ Microservices/Delivery-Tracking/src/main/java/com/project/delivery/delivery/tracking/infrastructure/event/IntegrationEventPublisher.java
@@ -0,0 +1,7 @@
+package com.project.delivery.delivery.tracking.infrastructure.event;
+
+
+public interface IntegrationEventPublisher {
+
+    void publish(Object event, String key, String topic);
+}
---
+++ Microservices/Service-Registre/src/test/java/com/project/delivery/serviceregistre/ServiceRegistreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.delivery.serviceregistre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
